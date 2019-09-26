2990ff4dd8e2b11cca6e75fa26212a407a408dec
Your Name <you@example.com>
2019-09-26 16:11:19 +0800

    first commit

---
+++ src/components/templates/customTemplate/template.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "bootstrap/dist/css/bootstrap.css";
+
+const Template = ({ document }) => (
+  <div className="container" style={{background: "pink"}}>
+    <h1>CERTIFICATE OF COMPLETION</h1>
+    <h2>{document.particpantName}</h2>
+  </div>
+);
+
+Template.propTypes = {
+  document: PropTypes.object.isRequired
+};
+
+export default Template;
