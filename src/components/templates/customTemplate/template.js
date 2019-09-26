368c063c026b46f4a24897040653f5ff34356ace
Your Name <you@example.com>
2019-09-26 16:38:42 +0800

    changetoblue

--- src/components/templates/customTemplate/template.js
+++ src/components/templates/customTemplate/template.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.css";
 
 const Template = ({ document }) => (
-  <div className="container" style={{background: "pink"}}>
+  <div className="container" style={{background: "blue"}}>
     <h1>CERTIFICATE OF COMPLETION</h1>
     <h2>{document.particpantName}</h2>
   </div>
